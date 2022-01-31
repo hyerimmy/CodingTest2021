f06421fd22d95f219a34fae5808fc9d854804e15
hyerimmy <heyrim2010@naver.com>
2022-01-31 15:46:11 +0000

    2022.01.31

---
+++ stepbystep/08/1712.py
@@ -0,0 +1,23 @@
+# 손익분기점
+# https://www.acmicpc.net/problem/1712
+
+a, b, c = map(int, input().split())
+
+result = -1
+n=int(a/c-b)
+print(n)
+
+if c<b:
+  print(result)
+else:
+  while True:
+    n+=1000
+    print(n, "/ a=b*n", a+b*n, "/ c*n", c*n)
+    if((a+b*n)<(c*n)):
+      while ((a+b*n)>=(c*n)):
+        result = n
+    else:
+      print(result)
+      quit()
+
+## 시간초과 왓더헬
---
+++ stepbystep/08/2292.py
@@ -0,0 +1,3 @@
+#벌집
+#https://www.acmicpc.net/problem/2292
+
