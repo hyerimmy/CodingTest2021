f809fa8ee2e7dae43f3f3d96bb9a61c7605f84ef
hyerimmy <heyrim2010@naver.com>
2022-05-21 06:48:17 +0000

    2022.05.21

--- stepbystep/07/2869.py
+++ stepbystep/07/2869.py
@@ -4,11 +4,22 @@ a, b, v = map(int, input().split())
 
 result = 1
 
-while(v>0):
-  v-=a
-  if(v<=0):
-    break;
-  v+=b
-  result+=1
+# while(v>0):
+#   v-=a
+#   if(v<=0):
+#     break;
+#   v+=b
+#   result+=1
+
+
+v-=a
+# while(v>0):
+#   v=v+b-a
+#   # print(str("result"),result,str("v"), v)
+#   result+=1
+
+import math
+temp = math.ceil(v/(a-b))
+result+=temp
 
 print(result)
