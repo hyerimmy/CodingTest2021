725c22017bb086dd36ece790cc092087ac5d91b8
hyerimmy <heyrim2010@naver.com>
2022-05-22 05:15:04 +0000

    2022.05.22

--- stepbystep/07/1712.py
+++ stepbystep/07/1712.py
@@ -3,21 +3,28 @@
 
 a, b, c = map(int, input().split())
 
-result = -1
-n=int(a/c-b)
-print(n)
+# result = -1
+# n=int(a/c-b)
+# print(n)
 
-if c<b:
-  print(result)
-else:
-  while True:
-    n+=1000
-    print(n, "/ a=b*n", a+b*n, "/ c*n", c*n)
-    if((a+b*n)<(c*n)):
-      while ((a+b*n)>=(c*n)):
-        result = n
-    else:
-      print(result)
-      quit()
-
+# if c<b:
+#   print(result)
+# else:
+#   while True:
+#     n+=1000
+#     print(n, "/ a=b*n", a+b*n, "/ c*n", c*n)
+#     if((a+b*n)<(c*n)):
+#       while ((a+b*n)>=(c*n)):
+#         result = n
+#     else:
+#       print(result)
+#       quit()
 ## 시간초과 왓더헬
+
+
+if c<=b:
+  result=-1
+else:
+  result = (a//(c-b))+1
+
+print(result)
